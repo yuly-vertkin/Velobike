5bef505ff907d5f8da99e34e1a8b6300ec90b829
Верткин Юлий Михайлович <Yuliy.Vertkin@russianpost.ru>
2024-05-13 20:25:14 +0300

    small refactoring

--- app/src/main/java/ru/sitronics/velobike/presentation/rent/WheelLockDialog.kt
+++ app/src/main/java/ru/sitronics/velobike/presentation/rent/WheelLockDialog.kt
@@ -1,12 +1,11 @@
 package ru.sitronics.velobike.presentation.rent
 
-import android.os.Build.VERSION.SDK_INT
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -18,36 +17,21 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import coil.ImageLoader
 import coil.compose.rememberAsyncImagePainter
-import coil.decode.GifDecoder
-import coil.decode.ImageDecoderDecoder
 import ru.sitronics.velobike.R
+import ru.sitronics.velobike.presentation.map.DialogState.CLOSE
+import ru.sitronics.velobike.presentation.map.DialogState.CLOSING
+import ru.sitronics.velobike.presentation.map.DialogState.SHOW
 import ru.sitronics.velobike.presentation.map.MapUiState
 import ru.sitronics.velobike.presentation.map.MapUiState.WheelLock
-import ru.sitronics.velobike.presentation.map.DialogState.SHOW
-import ru.sitronics.velobike.presentation.map.DialogState.CLOSING
-import ru.sitronics.velobike.presentation.map.DialogState.CLOSE
 import ru.sitronics.velobike.presentation.map.toDialogState
+import ru.sitronics.velobike.tools.getImageLoader
 
 @Composable
 fun BoxScope.WheelLockDialog(uiState: MapUiState, onClick: () -> Unit) {
     var state by remember { mutableStateOf(CLOSE) }
-    val context = LocalContext.current
-
-    val imageLoader = remember {
-        ImageLoader.Builder(context)
-            .components {
-                if (SDK_INT >= 28)
-                    add(ImageDecoderDecoder.Factory())
-                else
-                    add(GifDecoder.Factory())
-            }
-            .build()
-    }
 
     if (uiState is WheelLock && state != CLOSING) {
         state = true.toDialogState()
@@ -62,11 +46,9 @@ fun BoxScope.WheelLockDialog(uiState: MapUiState, onClick: () -> Unit) {
                 .padding(all = 12.dp)
         ) {
             Image(
-                painter = rememberAsyncImagePainter(R.drawable.close_wheel_lock, imageLoader),
+                painter = rememberAsyncImagePainter(R.drawable.close_wheel_lock, getImageLoader()),
                 contentDescription = null,
-                modifier = Modifier
-                    .width(300.dp)
-                    .height(300.dp)
+                modifier = Modifier.size(300.dp, 300.dp)
             )
             Button(
                 onClick = { state = CLOSING; onClick() },
